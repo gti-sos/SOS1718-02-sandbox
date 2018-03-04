dcf4bdb7de59125c28c35f8dc5a73d090a1c8f95
Andres Lorenzo Carvia <wirfen@gmail.com>
2018-03-04 12:25:51 +0000

    Organizando

---
+++ practicas/lab04/index.js
@@ -0,0 +1,10 @@
+var cool = require("cool-ascii-faces");
+var express = require("express");
+var app=express();
+
+app.get ("/hello",(req,res)=>{
+    res.send("Hello!");
+});
+
+app.listen(process.env.PORT);
+console.log(cool);
